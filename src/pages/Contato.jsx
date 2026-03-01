02f103392cf8c894f954e624ae7c64527b99f71c
Ademir Gomes <dmrademir@gmail.com>
2026-03-01 09:39:54 -0300

    Add: primeiro build after modo components.

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -26,6 +26,7 @@ export default function Contato() {
     // No seu handleSendEmail, substitua as strings fixas por:
     const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID; 
     const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    console.log("DEBUG KEY:", import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
     const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, formRef.current, PUBLIC_KEY)
